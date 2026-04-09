ce149b877229f98ac551a9e4114814e7ef8adb26
XSupremeYX <86472828+xsupremeyx@users.noreply.github.com>
2026-04-09 20:09:15 +0530

    feat: add custom Error handling for author.

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.use('/authors', authorRouter)
 app.use('/books', bookRouter)
 app.use('/', indexRouter)
 
+app.use((err,req,res,next) => {
+    // Error Middleware, always require all 4 parameters, even if you don't use them all
+    console.error(err);
+    res.status(err.statusCode || 500).send(err.message);
+});
+
 const PORT = 3000
 
 app.listen(PORT, (error) => {
--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,18 +1,21 @@
 const db = require('../db')
+const CustomNotFoundError = require('../errors/CustomNotFoundError')
 
-async function getAuthorById(req, res){
+
+async function getAuthorById(req, res, next){
     const { authorId } = req.params;
     try{
         const author = await db.getAuthorById(Number(authorId))
         if(!author){
-            res.status(404).send('Author not found');
-            return;
+            throw new CustomNotFoundError("Author not found");
         }
         res.send(`Author Name: ${author.name}`);
     }
     catch (error) {
-        console.error("Error fetching author:", error);
-        res.status(500).send("Internal Server Error");
+        // console.error("Error fetching author:", error);
+        // res.status(500).send("Internal Server Error");
+        // Pass the error to the next middleware (error handling middleware)
+        next(error);
     }
 };
 
