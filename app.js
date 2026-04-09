5a6b4368d1ecc266343d726722a2c5fcddc6304f
XSupremeYX <86472828+xsupremeyx@users.noreply.github.com>
2026-04-09 19:30:19 +0530

    feat: Add author controller, mock db.

--- app.js
+++ app.js
@@ -9,28 +9,6 @@ app.use('/authors', authorRouter)
 app.use('/books', bookRouter)
 app.use('/', indexRouter)
 
-
-// const path = './docs/'
-// app.get('/', (req, res) => {
-//     res.sendFile(path + 'index.html', { root: __dirname})
-// })
-
-// app.get('/about', (req, res) => {
-//     res.sendFile(path + 'about.html', { root: __dirname})
-// })
-
-// app.get('/contact-me', (req, res) => {
-//     res.sendFile(path + 'contact-me.html', { root: __dirname})
-// })
-
-// app.get('/contact', (req, res) => {
-//     res.redirect('/contact-me')
-// })
-
-// app.use((req, res) => {
-//     res.status(404).sendFile(path + '404.html', { root: __dirname})
-// })
-
 const PORT = 3000
 
 app.listen(PORT, (error) => {
--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,12 +1,11 @@
 // routes/authorRouter.js
 const { Router } = require("express");
 
+const { getAuthorById } =require('../controllers/authorController')
+
 const authorRouter = Router();
 
 authorRouter.get("/", (req, res) => res.send("All authors"));
-authorRouter.get("/:authorId", (req, res) => {
-  const { authorId } = req.params;
-  res.send(`Author ID: ${authorId}`);
-});
+authorRouter.get("/:authorId", getAuthorById)
 
 module.exports = authorRouter;
