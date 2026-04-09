c1d424ca0e84a850db42dac42e5f3f4b3bfc45c1
XSupremeYX <86472828+xsupremeyx@users.noreply.github.com>
2026-04-09 19:46:47 +0530

    feat: use trycatch error in authorController. showcase how to use trycatch error in authorController. not using Error handler middleware in express.

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -2,14 +2,18 @@ const db = require('../db')
 
 async function getAuthorById(req, res){
     const { authorId } = req.params;
-    const author = await db.getAuthorById(Number(authorId))
-
-    if(!author){
-        res.status(404).send('Author not found');
-        return;
+    try{
+        const author = await db.getAuthorById(Number(authorId))
+        if(!author){
+            res.status(404).send('Author not found');
+            return;
+        res.send(`Author Name: ${author.name}`);
+        }
     }
-
-    res.send(`Author Name: ${author.name}`);
+    catch (error) {
+        console.error("Error fetching author:", error);
+        res.status(500).send("Internal Server Error");
+    }
 };
 
 module.exports = { getAuthorById };
